cab60932e6c81c2d9a2167a4b872ea6d5ebfb671
raghu <raghumurukutla96@gmail.com>
2025-05-15 22:01:52 +0530

    array script added

---
+++ array-list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "First Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
+
+echo "All movies*: ${MOVIES[*]}"
+echo "All movies @: ${MOVIES[@]}"
+echo "All movies%: ${MOVIES[%]}"
