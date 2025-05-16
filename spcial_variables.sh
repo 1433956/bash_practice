df6ce0b22c4db1bf5d9467685b4a078cafd826d9
raghu <raghumurukutla96@gmail.com>
2025-05-16 21:45:44 +0530

    spacial operation script updated

--- spcial_variables.sh
+++ spcial_variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Special Variables
 
-echo "variables are " $@
-echo "variables are " $#s
-echo "script name is " $0
-echo "pid of the script" $$
+echo "variables are=" $@
+echo "variables are= " $#s
+echo "script name is=" $0
+echo "pid of the script=" $$
  sleep 5 &
-echo "pid of the last background  process" $!
-echo "exit status of the last command" $?
-echo "user" $user
-echo "home directory" $HOME
-echo "current working directory" $PWD
+echo "pid of the last background  process=" $!
+echo "exit status of the last command="$?
+echo "user=" $user
+echo "home directory=" $HOME
+echo "current working directory=" $PWD
