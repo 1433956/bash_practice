4ba3b3e580e3a8039df9846f11bff88a519424ff
raghu <raghumurukutla96@gmail.com>
2025-05-17 19:42:42 +0530

    my-sql user script added

--- mysql_install.sh
+++ mysql_install.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 #root user
-userid= $(id -u)
-if [ $userid -ne 0 ]
- then
- echo "you are not root user"
+# check if the script is run as root
+ user_id=$(id -u) 
+ #check user_id is root or not
+
+if [ $user_id -ne 0 ] then
+  echo "you are not root user, please run as root"
   exit 1
-else 
- echo "you are root user"
- 
-fi 
- dnf list installed mysql
 
- if [ $? -ne 0 ] then
-    echo "mysql is not installed"
+  else 
+   echo "you are root user"
+   fi
+   
+# check if mysql is installed
+   dnf list installed mysql
+
+   if [ $? -eq 0 ] then
+
+    echo "mysql is already installed"
+exit 1
+   else 
+  echo "mysql is not installed"
+
+# install mysql
     dnf install mysql -y
-else 
+fi
+    if [$? -eq 0 ] then
+      echo "mysql is installed successfully"
+    else
+      echo "mysql installation failed"
+      exit 1
+    fi
+    
 
-    echo "mysql is already installed" 
-    exit 1
-fi
-if [ $? -eq 0 ] then
-    echo "mysql is installed successfully"
-else
-    echo "mysql installation failed"
-    exit 1
-fi
+
