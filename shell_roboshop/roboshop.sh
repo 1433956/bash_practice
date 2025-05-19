5ef4455789d8cfadfa0192705b31a5d6c2bb98f7
raghu <raghumurukutla96@gmail.com>
2025-05-19 23:36:13 +0530

    roboshop script changes

--- shell_roboshop/roboshop.sh
+++ shell_roboshop/roboshop.sh
@@ -6,13 +6,14 @@ INSTANCES=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "cart" "user" "shipp
 Zone_ID="Z04391491005OGSNXNQQX"
 DOMAIN_NAME="devops73.site"
 
-  for instace in ${INSTANCES[@]}
+  for instance in ${INSTANCES[@]}
   do
-     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-09c3d9a208889e92c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instace}]" --query "Instances[0].InstanceId" --output text) 
-     if [ $instace !=  "frontend" ]
+     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-09c3d9a208889e92c --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+     #INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-09c3d9a208889e92c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instace}]" --query "Instances[0].InstanceId" --output text) 
+     if [ $instance !=  "frontend" ]
      then
-        IP=$(aws ec2 describe-instances --instance-ids $instace --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $instance --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
      else 
-        IP=$(aws ec2 describe-instances --instance-ids $instace --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $instinstanceace --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
      fi
   done
