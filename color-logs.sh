562b6307c974e854f483107bafde26cee17d1177
raghu <raghumurukutla96@gmail.com>
2025-05-19 20:42:17 +0530

    colors-log script changes

--- color-logs.sh
+++ color-logs.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
- R="\e[31m"
+userid=$(id -u)
+R="\e[31m"
  G="\e[32m"
  Y="\e[33m"
  N="\e[0m"
 
-userid=$(id -u)
-
  if [ $userid -ne 0 ]
  then
-      echo -e"$R You are not root user $N"
-      "
+      echo -e "$R You are not root user $N"
       exit 1
  else
-      echo -e $Y You are root user $N"
+      echo -e "$G You are root user $N"
  fi
